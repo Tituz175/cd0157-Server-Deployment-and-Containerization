f9fc03bf59d4fbda28df74f7ee61a596cd0dfd26
Tituz175 <tobititus01@gmail.com>
2022-11-26 11:41:22 +0100

    project review 2 addressed once again

--- test_main.py
+++ test_main.py
@@ -25,7 +25,7 @@ def client():
 def test_health(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert response.json == 'Healthy'
+    assert response.json == 'I might run out of text messages to send you, and I might run out of jokes too. My phone might run out of battery, but my heart will never run out of space for you.'
 
 
 def test_auth(client):
